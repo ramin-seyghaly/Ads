7d5e4320579ac091b4ec547a13b20cd575b7dbed
ramin seyghaly <ramin.seyghaly.developer@gmail.com>
2021-01-14 12:27:24 +0330

    Initial commit

---
+++ ads/src/main/java/ramin/seyghaly/ads/core/AdsObserver.java
@@ -0,0 +1,25 @@
+package ramin.seyghaly.ads.core;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class AdsObserver implements Observer {
+
+    private Delegate delegate;
+
+    public interface Delegate{
+        void onActionEvent(AdsModel adsModel);
+    }
+
+    public AdsObserver(Delegate actionDelegate) {
+        this.delegate = actionDelegate;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if (arg instanceof AdsModel) {
+            delegate.onActionEvent((AdsModel) arg);
+        }
+    }
+
+}
